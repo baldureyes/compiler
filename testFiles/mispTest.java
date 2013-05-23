2ab31c879743d187b18cf5d1b8309e772d244330
j129008 <j129008@gmail.com>
2013-05-23 16:13:16 +0800

    new file:   testFiles/mispTest.java add mispTest file modified:   Makefile make clean delete mispTest file
    modified:   parser.y
    finish init part // .data .text ....

---
+++ testFiles/mispTest.java
@@ -0,0 +1,14 @@
+class Testcase{
+   public static void main(String[] a){
+      System.out.println(222);
+   }
+}
+
+class CS{
+   int a;
+   public int test(){
+      a=5;
+      System.out.println(a); 
+      return 0;
+   }
+}
