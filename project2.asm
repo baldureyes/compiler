3b41392540663132f3cb3bf688a58643c32e43fe
j129008 <j129008@gmail.com>
2013-05-26 03:13:10 +0800

    add outfile

--- project2.asm
+++ project2.asm
@@ -2,5 +2,17 @@
    .text
    .global main
 main:
+   li $a0,222
+   li $v0,1
+   syscall
    .data
    .text
+   # init param dec
+   li $t0,0
+   sw $t0,0($sp)
+   # init param dec
+   li $t0,0
+   sw $t0,4($sp)
+   lw $a0,0($sp)
+   li $v0,1
+   syscall
